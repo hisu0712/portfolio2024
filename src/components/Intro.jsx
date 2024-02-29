9f014d253662ffa0a2b8d332e6dc9ab5e814af1f
heesu <vbnm0712@naver.com>
2024-02-29 18:52:02 +0900

    인트로 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+
+import about from "../assets/img/about.png";
+
+const introText = {
+  title: "web developer",
+  desc: ["talnet is", "found at the end of the", "effort"]
+}
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <section id="intro">
+      <div className="intro__inner">
+        <h1 className="intro__title">
+          {introText.title}
+        </h1>
+        <div className="intro__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="intro__text">
+          <div className="text">
+            <div>{introText.desc[0]}</div>
+            <div>{introText.desc[1]}</div>
+            <div>{introText.desc[2]}</div>
+          </div>
+          <div className="img">
+            <img src={about} alt="어바웃" />
+          </div>
+        </div>
+        <div className="intro__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
   )
 }
 
