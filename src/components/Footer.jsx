a682305c5b41367e7257a867bf3326711be3a51a
heesu <vbnm0712@naver.com>
2024-03-01 15:34:53 +0900

    사이트 영역 완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="dk">
+<div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
+    <div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
+    <div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
+    <div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
+    <div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
+    </div>
   )
 }
 
