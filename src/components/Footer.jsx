7651a716f28f58008a0e18edf2140fb658f86314
heesu <vbnm0712@naver.com>
2024-03-04 14:47:21 +0900

    푸터 영역 완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,62 @@
 import React from 'react'
 
+const footerText = [
+  {
+    link : "/",
+    title : "phone",
+    desc : "010-2821-5798"
+  },
+  {
+    link : "/",
+    title : "mail",
+    desc : "vbnm0712@naver.com"
+  },
+  {
+    link : "/",
+    title : "github",
+    desc : "깃헙에 들어오시면 모든 소스를 볼 수 있습니다."
+  },  
+  {
+    link : "/",
+    title : "github",
+    desc : "깃헙에 들어오시면 모든 소스를 볼 수 있습니다."
+  },
+  {
+    link : "/",
+    title : "phone",
+    desc : "010-2821-5798"
+  },
+];
+
 const Footer = () => {
   return (
-    <div className="dk">
-<div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
-    <div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
-    <div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
-    <div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
-    <div>Footer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque alias doloremque, debitis beatae tenetur quibusdam placeat voluptatum omnis consequatur perspiciatis commodi iure! Perspiciatis amet dolores nisi assumenda perferendis accusamus dolorum?</div>
-    </div>
+    <footer className="footer" role="contentinfo">
+      <div className="footer__inner">
+        <div className="footer__info">
+          <div className="left">
+            <div className="title">
+              <a href="/">notion</a>
+            </div>
+            <p>노션으로 들어가시면 더 많은 정보를 볼 수 있습니다.</p>
+          </div>
+          <div className="right">
+            <h3>social</h3>
+            <ul>
+              {footerText.map((footer, key) => (
+                <li key={key}>
+                  <a href={footer.link}>{footer.title}</a>
+                  <em>{footer.desc}</em>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="footer__right">
+          © 2024 ShinHeesu<br />
+          이 사이트는 리액트를 이용하여 제작하였습니다.
+        </div>
+      </div>
+    </footer>
   )
 }
 
